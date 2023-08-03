89fb768df08acd70909adfc6e517a7db725834d2
Teddy <dudetotheleft@gmail.com>
2023-08-03 14:02:05 -0700

    Renamed initial form

--- TeamSourceControl/Forms/MovieManager.Designer.cs
+++ TeamSourceControl/Forms/MovieManager.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TeamSourceControl
 {
-    partial class Form1
+    partial class MovieManager
     {
         /// <summary>
         ///  Required designer variable.
--- TeamSourceControl/Forms/MovieManager.cs
+++ TeamSourceControl/Forms/MovieManager.cs
@@ -1,8 +1,8 @@
 namespace TeamSourceControl
 {
-    public partial class Form1 : Form
+    public partial class MovieManager : Form
     {
-        public Form1()
+        public MovieManager()
         {
             InitializeComponent();
         }
