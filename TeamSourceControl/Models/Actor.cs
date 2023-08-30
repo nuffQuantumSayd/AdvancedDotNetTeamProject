8b4567f60db5e65a1d80bc2cf68af89792446830
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-30 14:33:57 -0700

    Documented Actor class

--- TeamSourceControl/Models/Actor.cs
+++ TeamSourceControl/Models/Actor.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 
 namespace TeamSourceControl.Models;
 
+/// <summary>
+/// An actor 
+/// </summary>
 public partial class Actor
 {
+    /// <summary>
+    /// The actors Id
+    /// </summary>
     public int ActorId { get; set; }
-
+    /// <summary>
+    /// The actors first name
+    /// </summary>
     public string ActorFirstName { get; set; } = null!;
-
+    /// <summary>
+    /// The actors last name
+    /// </summary>
     public string ActorLastName { get; set; } = null!;
-
+    /// <summary>
+    /// The actors following
+    /// </summary>
     public bool ActorFollowed { get; set; }
 }
