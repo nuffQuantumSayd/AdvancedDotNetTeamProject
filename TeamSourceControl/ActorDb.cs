3308540366f592c77750e41b9defc3174a360900
Teddy <dudetotheleft@gmail.com>
2023-08-31 17:19:38 -0700

    ActorDb method layout using NIExceptions (wasn't committed earlier for some reason?)

--- TeamSourceControl/ActorDb.cs
+++ TeamSourceControl/ActorDb.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamSourceControl.Models;
 
 namespace TeamSourceControl
 {
-    internal class ActorDb
+    static class ActorDb
     {
+        public static void Add(Actor a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Update(Actor a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Delete(Actor a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Actor> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Actor Get(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
